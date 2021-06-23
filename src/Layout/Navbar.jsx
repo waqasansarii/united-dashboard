8308a007a106169be1a233566650fdc3e3cbd84a
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-06-23 18:18:52 +0500

    updated

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,34 +1,58 @@
 import React from "react";
-import {NavLink} from 'react-router-dom'
-import {Navbar,Nav} from 'react-bootstrap'
-import search from '../Assets/Path 19.png'
-import lines from '../Assets/Group 34.png'
-import user from '../Assets/Ellipse 1.png'
-import './style.css'
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import search from "../Assets/Path 19.png";
+import lines from "../Assets/Group 34.png";
+import user from "../Assets/Ellipse 1.png";
+import "./style.css";
 
 const NavbarMain = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className='custom_nav' bg='dar' variant="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="custom_nav"
+        bg="dar"
+        variant="dark"
+      >
         <Navbar.Brand href="#home">LOGO</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto secnd_nav_ul">
-            <NavLink className='nav-link' exact to="/" >Traffic</NavLink>
-            <NavLink className='nav-link' exact to="/analytic">Analytics</NavLink>
-            <NavLink className='nav-link' to="pricing">Overview</NavLink>
-            <NavLink className='nav-link' to="pricing">Dashboard</NavLink>
+            <ul>
+              <li>
+                <NavLink activeClassName='active_link' className="nav-link" exact to="/">
+                  Traffic
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName='active_link' className="nav-link" exact to="/analytic">
+                  Analytics
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName='active_link' className="nav-link" to="/Overview">
+                  Overview
+                </NavLink>
+              </li>
+              <li>
+                <NavLink activeClassName='active_link' className="nav-link" to="/Dashboard">
+                  Dashboard
+                </NavLink>
+              </li>
+            </ul>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">
-              <img className='nav_icons' src={search} alt="..." />
-              </Nav.Link>
+              <img className="nav_icons" src={search} alt="..." />
+            </Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-            <img className='nav_icons' src={lines} alt="..." />
+              <img className="nav_icons" src={lines} alt="..." />
             </Nav.Link>
             <Nav.Link href="#deets">
               <img src={user} alt="..." />
-              </Nav.Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
